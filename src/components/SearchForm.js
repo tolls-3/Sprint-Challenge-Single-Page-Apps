0de9dfc4ea1f097afb779b7f999dd02d649192bf
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-09-20 11:17:42 +0100

    Final submission, still working on search form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -9,32 +9,3 @@ export default function SearchForm(props) {
   );
 }
 
-// import React, { useState } from "react";
-
-// export default function SearchForm(props) {
-//   const [searchValue, setSearchValue] = useState("");
-
-//   const onInputChange = (e) => {
-//     setSearchValue(e.target.value);
-//   }
-//   const callSearchFunction = (e) => {
-//     e.preventDefault();
-    
-//   }
-
-//   return (
-//     <section className="search-form">
-//       <form>
-//         <label htmlFor="name">Search by name: </label>
-//         <input
-//           type="text"
-//           name="name"
-//           value={searchValue}
-//           id="nameinput"
-//           onChange={onInputChange}
-//         />
-//         <button onClick={callSearchFunction}>Search</button>
-//       </form>
-//     </section>
-//   );
-// }
