e05252d6ff32e9c46f1eb60379bca6c8678e91d4
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-09-20 11:39:26 +0100

    Final submission, still working on search form

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import Header from "./components/Header.js";
 import WelcomePage from "./components/WelcomePage.js";
 import CharacterList from "./components/CharacterList";
 import { Route } from "react-router-dom";
+import styled from "styled-components";
 import SearchForm from './components/SearchForm';
+
 export default function App() {
   return (
-    <main>
+    <MainStyle>
       <Header />
       <Route exact path="/" component={WelcomePage} />
       <Route exact path="/characters" component={CharacterList} />
       <Route path='/search' component={SearchForm} />
-    </main>
+    </MainStyle>
   );
 }
+
+const MainStyle = styled.main`
+    background: yellow;`
+    
+   
