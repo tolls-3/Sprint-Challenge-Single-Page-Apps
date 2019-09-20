66e1d09968c9f1d1a1ccdfcc00395c5f4bc37f05
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-09-20 09:09:36 +0100

    done with charactercard and characterlist, also imported into app.js

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,16 @@
 import React from "react";
+import styled from 'styled-components';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return(
+  <div>
+    <img src={props.image}/>
+    <h2>{props.name}</h2>
+    <p>{props.status}</p>
+    <p>{props.species}</p>
+    <p>{props.gender}</p>
+    Location: {props.location.name}
+    Origin: {props.origin.name}
+  </div>
+  )
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,49 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import styled from "styled-components";
+import CharacterCard from "./CharacterCard";
+
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
 
+  const [character,setCharacter]=useState([]);
+  const [error, setError]=useState('');
+  const characterApi = 'https://rickandmortyapi.com/api/character/';
+  const backupApi = 'https://rick-api.herokuapp.com/api/character';
+
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get(characterApi)
+      .then(response => {
+        // debugger;
+        setCharacter(response.data.results);
+      })
+
+      .catch(error =>{
+
+      })
+  
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {
+        character.map(star => {
+          return(
+          <CharacterCard 
+          key={star.id} 
+          name={star.name} 
+          image={star.image}
+          status={star.status}
+          species={star.species}
+          gender={star.gender}
+          origin={star.origin}
+          location={star.location}/>
+          )
+        })
+      }
     </section>
   );
 }
