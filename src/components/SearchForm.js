1736ddad13f6c84349ca9b3c1259853c7944bc1a
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2019-09-20 11:20:14 +0100

    Final submission, still working on search form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -4,7 +4,6 @@ export default function SearchForm(props) {
   return (
     <section className="search-form">
       <input type="search" onChange={props.onSearch} />
-      {/* still working on search form */}
     </section>
   );
 }
